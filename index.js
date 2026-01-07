bec3afeeb6c5518e00c2d6b6626a246d1cbfb5e7
thisa93 <thisa93@gmail.com>
2026-01-07 17:10:07 +0530

    first Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import bodyParser from "body-parser";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import authRoutes from "./routes/auth.js";
+
+const app = express();
+const PORT = process.env.PORT;
+const MONGOURL = process.env.MONGOURL;
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use("/api", authRoutes);
+
+mongoose.connect(MONGOURL).then(()=>{
+    console.log("Database connected Successfully");
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`);
+    })
+}).catch((error)=>{
+    console.log(`Database connection failed: ${error}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,49 @@
+import express from "express";
+//import router from express.Router();
+const router = express.Router();
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
+import User from "../model/userModel.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+//Register
+router.post("/register",async(req,res)=>{
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password,10);
+
+        const user = new User({
+            userName: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        await user.save();
+        res.status(201).json("User Registered Successfully");
+    } catch(error){
+        res.status(500).json(error.message);
+    }
+});
+
+//Login
+router.post("/login", async(req,res)=>{
+    try{
+        const user = await User.findOne({email:req.body.email})
+        if(!user) return res.status(400).json("User not found");
+
+        const validPassword = await bcrypt.compare(req.body.password,user.password);
+
+        if(!validPassword) return res.status(400).json("Wrong Password");
+
+        const token = jwt.sign(
+            {id: user._id},
+            process.env.JWTSECRET,
+            {expiresIn: "1d"}
+        );
+
+        res.json({token, user});
+    } catch(error){
+        res.status(500).json(error.message);
+    }
+});
+
+export default router;
